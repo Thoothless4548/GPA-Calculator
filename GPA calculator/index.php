c9b776b4869f7ac7b041798b1f56d9a9dd907187
Anagha Shesware <91543524+Thoothless4548@users.noreply.github.com>
2024-01-20 15:49:43 +0530

    Add files via upload

---
+++ GPA calculator/index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>IT Gpa calculator</title>
+  </head>
+  <body style="background-color: #484848">
+
+    <div class="container bg-dark mx-auto" style="width: 80%;">
+      <!-- Content here -->
+<h3 class="text-white bg-dark" style = "font-family:courier,arial,helvetica;text-align: center;"> Welcome to Gpa calculator For IT Department </h3>
+<p class="text-right">
+<a class="text-light text-right" href="index.php"/>HOME</a>
+</p>
+<div class="text-white pl-5 lead font-weight-normal pr-5 pt-2">
+<p>
+  This is a Gpa calculator that calculates the gpa of the Student with the grades provided.
+  This is created using php based on Anna University R2017 regulation credits.
+  You would obtain the gpa for the grades entered and the credits earned for the Semester with which you will be
+  able to caculate cgpa . The sum of all such earnedcredits divided by sum of all credits gives CGPA
+</p>
+<p class="text-center">Select the Semester</p>
+<div class="center text-center" id="btn-url-links">
+<p>  <button type="button" class="btn btn-light" onclick="location.href = 'Sem1.php';">Semester 1</button></p>
+<p>  <button type="button" class="btn btn-light" onclick="location.href = 'Sem2.php';">Semester 2</button></p>
+<p>  <button type="button" class="btn btn-light" onclick="location.href = 'Sem3.php';">Semester 3</button></p>
+<p>  <button type="button" class="btn btn-light" onclick="location.href = 'Sem4.php';">Semester 4</button></p>
+<p>  <button type="button" class="btn btn-light" onclick="location.href = 'Sem5.php';">Semester 5</button></p>
+<p>  <button type="button" class="btn btn-light" onclick="location.href = 'Sem6.php';">Semester 6</button></p>
+<p>  <button type="button" class="btn btn-light" onclick="location.href = 'Sem7.php';">Semester 7</button></p>
+<p>  <button type="button" class="btn btn-light" onclick="location.href = 'Sem8.php';">Semester 8</button></p>
+</div>
